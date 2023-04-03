59eeba9e9deead10ec694ca42a63abc9785e6f95
Patrick Bloebaum <bloebp@amazon.com>
2023-04-03 10:26:47 -0700

    Add basic files
    Signed-off-by: Patrick Bloebaum <bloebp@amazon.com>

---
+++ p_value_result.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Optional, Union
+
+import numpy as np
+
+
+@dataclass
+class PValueResult:
+    """This data class represents the results of an hypothesis test that produces a p-value.
+
+    Attributes:
+    -----------
+    p_value: float
+        The p-value represents the probability of observing the given test statistic, or more extreme results, under a
+        certain null hypothesis.
+    test_statistic: float or numpy.ndarray or None
+        The test statistic of the hypothesis test, which might not always be available.
+    additional_information: object or None
+        Any additional information or metadata relevant to the specific test conducted. These could also be a state of
+        the method to re-use it.
+    """
+
+    p_value: float
+    test_statistic: Optional[Union[float, np.ndarray]] = None
+    additional_information: Optional[object] = None
