16b61edfd831ca0e7dc180605955cc6fe7b495ee
Adam Li <adam2392@gmail.com>
2023-07-13 16:20:04 -0400

    Adding categorical test
    Signed-off-by: Adam Li <adam2392@gmail.com>

---
+++ pywhy_stats/categorical.py
@@ -0,0 +1,197 @@
+"""Independence test among categorical variables using power-divergence tests.
+
+Works on categorical random variables. Based on the ``lambda_`` parameter, one
+can compute a wide variety of different categorical hypothesis tests.
+
+Examples
+--------
+>>> import pywhy_stats as ps
+>>> res = ps.categorical.ind([1, 2, 3], [4, 5, 6])
+>>> print(res.pvalue)
+>>> 1.0
+"""
+
+import logging
+from typing import Optional
+
+import numpy as np
+from numpy.typing import ArrayLike
+from scipy import stats
+
+from .pvalue_result import PValueResult
+
+
+def ind(X: ArrayLike, Y: ArrayLike, lambda_: str='cressie-read') -> PValueResult:
+    """Perform an independence test using power divergence test.
+
+    Parameters
+    ----------
+    X : ArrayLike of shape (n_samples,)
+        The first node variable.
+    Y : ArrayLike of shape (n_samples,)
+        The second node variable.
+    lambda_: float or string
+        The lambda parameter for the power_divergence statistic. Some values of
+        lambda_ results in other well known tests:
+            "pearson"             1          "Chi-squared test"
+            "log-likelihood"      0          "G-test or log-likelihood"
+            "freeman-tukey"     -1/2        "freeman-tukey Statistic"
+            "mod-log-likelihood"  -1         "Modified Log-likelihood"
+            "neyman"              -2         "Neyman's statistic"
+            "cressie-read"        2/3        "The value recommended in the paper
+                                             :footcite:`cressieread1984`"
+
+    Returns
+    -------
+    statistic : float
+        The test statistic.
+    pvalue : float
+        The p-value of the test.
+
+    References
+    ----------
+    .. footbibliography::
+    """
+    return _power_divergence(X=X, Y=Y, Z=None, lambda_=lambda_)
+
+
+def condind(X: ArrayLike, Y: ArrayLike, condition_on: ArrayLike, lambda_: str='cressie-read') -> PValueResult:
+    """Perform an independence test using power divergence test.
+
+    Parameters
+    ----------
+    X : ArrayLike of shape (n_samples,)
+        The first node variable.
+    Y : ArrayLike of shape (n_samples,)
+        The second node variable.
+    condition_on : ArrayLike of shape (n_samples, n_variables)
+        The conditioning set.
+    lambda_: float or string
+        The lambda parameter for the power_divergence statistic. Some values of
+        lambda_ results in other well known tests:
+            "pearson"             1          "Chi-squared test"
+            "log-likelihood"      0          "G-test or log-likelihood"
+            "freeman-tukey"     -1/2        "freeman-tukey Statistic"
+            "mod-log-likelihood"  -1         "Modified Log-likelihood"
+            "neyman"              -2         "Neyman's statistic"
+            "cressie-read"        2/3        "The value recommended in the paper
+                                             :footcite:`cressieread1984`"
+
+    Returns
+    -------
+    statistic : float
+        The test statistic.
+    pvalue : float
+        The p-value of the test.
+    """
+    return _power_divergence(X=X, Y=Y, Z=condition_on, lambda_=lambda_)
+
+#    # Step 1: Check if the arguments are valid and type conversions.
+#     if isinstance(Z, str):
+#         Z = [Z]
+#     if (X in Z) or (Y in Z):
+#         raise ValueError(f"The variables X or Y can't be in Z. Found {X if X in Z else Y} in Z.")
+
+
+# This is a modified function taken from pgmpy: License MIT
+def _power_divergence(
+    X: ArrayLike, Y: ArrayLike, Z: Optional[ArrayLike], lambda_: str = "cressie-read"
+) -> PValueResult:
+    """Computes the Cressie-Read power divergence statistic [1].
+    
+    The null hypothesis for the test is X is independent of Y given Z. A lot of the
+    frequency comparison based statistics (eg. chi-square, G-test etc) belong to
+    power divergence family, and are special cases of this test.
+
+    Parameters
+    ----------
+    X: ArrayLike of shape (n_samples,)
+        The first node variable.
+    Y : ArrayLike of shape (n_samples,)
+        The second node variable.
+    Z : ArrayLike of shape (n_samples, n_variables)
+        The conditioning set.
+    lambda_: float or string
+        The lambda parameter for the power_divergence statistic. Some values of
+        lambda_ results in other well known tests:
+            "pearson"             1          "Chi-squared test"
+            "log-likelihood"      0          "G-test or log-likelihood"
+            "freeman-tukey"     -1/2        "freeman-tukey Statistic"
+            "mod-log-likelihood"  -1         "Modified Log-likelihood"
+            "neyman"              -2         "Neyman's statistic"
+            "cressie-read"        2/3        "The value recommended in the paper
+                                             :footcite:`cressieread1984`"
+
+    Returns
+    -------
+    CI Test Results: tuple
+        Returns a tuple (chi, p_value, dof). `chi` is the
+        chi-squared test statistic. The `p_value` for the test, i.e. the
+        probability of observing the computed chi-square statistic (or an even
+        higher value), given the null hypothesis that X \u27C2 Y | Zs is True.
+        If boolean = True, returns True if the p_value of the test is greater
+        than `significance_level` else returns False.
+
+    See Also
+    --------
+    scipy.stats.power_divergence
+
+    References
+    ----------
+    .. footbibliography::
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> data = pd.DataFrame(np.random.randint(0, 2, size=(50000, 4)), columns=list('ABCD'))
+    >>> data['E'] = data['A'] + data['B'] + data['C']
+    >>> chi_square(X='A', Y='C', Z=[], data=data, boolean=True, significance_level=0.05)
+    True
+    >>> chi_square(X='A', Y='B', Z=['D'], data=data, boolean=True, significance_level=0.05)
+    True
+    >>> chi_square(X='A', Y='B', Z=['D', 'E'], data=data, boolean=True, significance_level=0.05)
+    False
+    """
+    # Step 1: Do a simple contingency test if there are no conditional variables.
+    if Z is None:
+        # XXX: convert this to work with numpy arrays
+        chi, p_value, dof, expected = stats.chi2_contingency(
+            data.groupby([X, Y]).size().unstack(Y, fill_value=0), lambda_=lambda_
+        )
+
+    # Step 2: If there are conditionals variables, iterate over unique states and do
+    #         the contingency test.
+    else:
+        chi = 0
+        dof = 0
+        for idx, (z_state, df) in enumerate(data.groupby(Z[0] if len(Z) == 1 else Z)):
+            try:
+                # Note: The fill value is set to 1e-7 to avoid the following error:
+                # where there are not enough samples in the data, which results in a nan pvalue
+                sub_table_z = df.groupby([X, Y]).size().unstack(Y, fill_value=1e-7)
+                c, _, d, _ = stats.chi2_contingency(sub_table_z, lambda_=lambda_)
+                chi += c
+                dof += d
+            except ValueError:
+                # If one of the values is 0 in the 2x2 table.
+                if isinstance(z_state, str):
+                    logging.info(
+                        f"Skipping the test {X} \u27C2 {Y} | {Z[idx]}={z_state}. Not enough samples"
+                    )
+                else:
+                    z_str = ", ".join([f"{var}={state}" for var, state in zip(Z, z_state)])
+                    logging.info(f"Skipping the test {X} \u27C2 {Y} | {z_str}. Not enough samples")
+
+            if np.isnan(c):
+                raise RuntimeError(
+                    f"The resulting chi square test statistic is NaN, which occurs "
+                    f"when there are not enough samples in your data "
+                    f"{df.shape}, {sub_table_z}."
+                )
+
+        p_value = 1 - stats.chi2.cdf(chi, df=dof)
+
+    # Step 4: Return the values
+    result = PValueResult(statistic=chi, pvalue=p_value, dof=dof)
+    return result
---
+++ tests/test_categorical.py
@@ -0,0 +1,295 @@
+import numpy as np
+import pandas as pd
+import pytest
+from numpy.testing import assert_almost_equal
+
+from dodiscover.ci import CategoricalCITest
+from math import frexp
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from dodiscover.ci import GSquareCITest
+from dodiscover.testdata import testdata
+
+seed = 12345
+
+df_adult = pd.read_csv("tests/testdata/adult.csv")
+
+
+def test_chisquare_marginal_independence_adult_dataset():
+    """Test that chi-square tests return the correct answer for marginal independence queries.
+
+    Uses the test data from dagitty.
+    """
+    # Comparision values taken from dagitty (DAGitty)
+    ci_est = CategoricalCITest("pearson")
+    coef, p_value = ci_est.test(x_vars={"Age"}, y_vars={"Immigrant"}, z_covariates=[], df=df_adult)
+    assert_almost_equal(coef, 57.75, decimal=1)
+    assert_almost_equal(np.log(p_value), -25.47, decimal=1)
+    assert ci_est.dof_ == 4
+
+    coef, p_value = ci_est.test(x_vars={"Age"}, y_vars={"Race"}, z_covariates=[], df=df_adult)
+    assert_almost_equal(coef, 56.25, decimal=1)
+    assert_almost_equal(np.log(p_value), -24.75, decimal=1)
+    assert ci_est.dof_ == 4
+
+    coef, p_value = ci_est.test(x_vars={"Age"}, y_vars={"Sex"}, z_covariates=[], df=df_adult)
+    assert_almost_equal(coef, 289.62, decimal=1)
+    assert_almost_equal(np.log(p_value), -139.82, decimal=1)
+    assert ci_est.dof_ == 4
+
+    coef, p_value = ci_est.test(x_vars={"Immigrant"}, y_vars={"Sex"}, z_covariates={}, df=df_adult)
+    assert_almost_equal(coef, 0.2724, decimal=1)
+    assert_almost_equal(np.log(p_value), -0.50, decimal=1)
+    assert ci_est.dof_ == 1
+
+
+def test_chisquare_conditional_independence_adult_dataset():
+    """Test that chi-square tests return the correct answer for conditional independence queries.
+
+    Uses the test data from dagitty.
+    """
+    ci_est = CategoricalCITest("pearson")
+
+    coef, p_value = coef, p_value = ci_est.test(
+        x_vars={"Education"},
+        y_vars={"HoursPerWeek"},
+        z_covariates=["Age", "Immigrant", "Race", "Sex"],
+        df=df_adult,
+    )
+    assert_almost_equal(coef, 1460.11, decimal=1)
+    assert_almost_equal(p_value, 0, decimal=1)
+    assert ci_est.dof_ == 316
+
+    coef, p_value = ci_est.test(
+        x_vars={"Education"}, y_vars={"MaritalStatus"}, z_covariates=["Age", "Sex"], df=df_adult
+    )
+    assert_almost_equal(coef, 481.96, decimal=1)
+    assert_almost_equal(p_value, 0, decimal=1)
+    assert ci_est.dof_ == 58
+
+    # Values differ (for next 2 tests) from dagitty because dagitty ignores grouped
+    # dataframes with very few samples. Update: Might be same from scipy_vars=1.7.0
+    coef, p_value = ci_est.test(
+        x_vars={"Income"},
+        y_vars={"Race"},
+        z_covariates=["Age", "Education", "HoursPerWeek", "MaritalStatus"],
+        df=df_adult,
+    )
+
+    assert_almost_equal(coef, 66.39, decimal=1)
+    assert_almost_equal(p_value, 0.99, decimal=1)
+    assert ci_est.dof_ == 136
+
+    coef, p_value = ci_est.test(
+        x_vars={"Immigrant"},
+        y_vars={"Income"},
+        z_covariates=["Age", "Education", "HoursPerWeek", "MaritalStatus"],
+        df=df_adult,
+    )
+    assert_almost_equal(coef, 65.59, decimal=1)
+    assert_almost_equal(p_value, 0.999, decimal=2)
+    assert ci_est.dof_ == 131
+
+
+@pytest.mark.parametrize(
+    "ci_test",
+    [
+        CategoricalCITest("pearson"),  # chi-square
+        CategoricalCITest("log-likelihood"),  # G^2
+        CategoricalCITest("freeman-tukey"),  # freeman-tukey
+        CategoricalCITest("mod-log-likelihood"),  # Modified log-likelihood
+        CategoricalCITest("neyman"),  # Neyman
+        CategoricalCITest("cressie-read"),  # Cressie-read
+    ],
+)
+def test_chisquare_when_dependent(ci_test):
+    assert (
+        ci_test.test(
+            x_vars={"Age"},
+            y_vars={"Immigrant"},
+            z_covariates=[],
+            df=df_adult,
+        )[1]
+        < 0.05
+    )
+
+    assert (
+        ci_test.test(
+            x_vars={"Age"},
+            y_vars={"Race"},
+            z_covariates=[],
+            df=df_adult,
+        )[1]
+        < 0.05
+    )
+
+    assert (
+        ci_test.test(
+            x_vars={"Age"},
+            y_vars={"Sex"},
+            z_covariates=[],
+            df=df_adult,
+        )[1]
+        < 0.05
+    )
+    assert (
+        ci_test.test(
+            x_vars={"Immigrant"},
+            y_vars={"Sex"},
+            z_covariates=[],
+            df=df_adult,
+        )[1]
+        >= 0.05
+    )
+
+    assert (
+        ci_test.test(
+            x_vars={"Education"},
+            y_vars={"HoursPerWeek"},
+            z_covariates=["Age", "Immigrant", "Race", "Sex"],
+            df=df_adult,
+        )[1]
+        < 0.05
+    )
+    assert (
+        ci_test.test(
+            x_vars={"Education"},
+            y_vars={"MaritalStatus"},
+            z_covariates=["Age", "Sex"],
+            df=df_adult,
+        )[1]
+        < 0.05
+    )
+
+
+@pytest.mark.parametrize(
+    "ci_test",
+    [
+        CategoricalCITest("pearson"),  # chi-square
+        CategoricalCITest("log-likelihood"),  # G^2
+        CategoricalCITest("freeman-tukey"),  # freeman-tukey
+        CategoricalCITest("mod-log-likelihood"),  # Modified log-likelihood
+        CategoricalCITest("neyman"),  # Neyman
+        CategoricalCITest("cressie-read"),  # Cressie-read
+    ],
+)
+def test_chisquare_when_exactly_dependent(ci_test):
+    x = np.random.choice([0, 1], size=1000)
+    y = x.copy()
+    df = pd.DataFrame({"x": x, "y": y})
+
+    stat, p_value = ci_test.test(x_vars={"x"}, y_vars={"y"}, z_covariates=[], df=df)
+    assert ci_test.dof_ == 1
+    assert_almost_equal(p_value, 0, decimal=5)
+
+
+
+def test_g_discrete():
+    """Test G^2 test for discrete data."""
+    dm = np.array([testdata.dis_data]).reshape((10000, 5))
+    x = 0
+    y = 1
+    ci_estimator = GSquareCITest(data_type="discrete", levels=[3, 2, 3, 4, 2])
+    df = pd.DataFrame.from_records(dm)
+
+    sets = [[], [2], [2, 3], [3, 4], [2, 3, 4]]
+    for idx in range(len(sets)):
+        _, p = ci_estimator.test(df, {x}, {y}, set(sets[idx]))
+        fr_p = frexp(p)
+        fr_a = frexp(testdata.dis_answer[idx])
+
+        # due to adding small perturbation to prevent dividing by 0 within
+        # G^2 statistic computation
+        assert round(fr_p[0] - fr_a[0], 3) == 0
+        assert fr_p[1] == fr_a[1]
+        assert fr_p[0] > 0
+
+    # check error message for number of samples
+    dm = np.array([testdata.dis_data]).reshape((2000, 25))
+    df = pd.DataFrame.from_records(dm)
+    levels = np.ones((25,)) * 3
+    ci_estimator = GSquareCITest(data_type="discrete", levels=levels)
+    sets = [[2, 3, 4, 5, 6, 7]]
+    with pytest.raises(RuntimeError, match="Not enough samples"):
+        ci_estimator.test(df, {x}, {y}, set(sets[0]))
+
+
+def test_g_binary():
+    """Test G^2 test for binary data."""
+    dm = np.array([testdata.bin_data]).reshape((5000, 5))
+    x = 0
+    y = 1
+    ci_estimator = GSquareCITest(data_type="binary")
+    df = pd.DataFrame.from_records(dm)
+
+    sets = [[], [2], [2, 3], [3, 4], [2, 3, 4]]
+    for idx in range(len(sets)):
+        _, p = ci_estimator.test(df, {x}, {y}, set(sets[idx]))
+        fr_p = frexp(p)
+        fr_a = frexp(testdata.bin_answer[idx])
+        assert fr_p[1] == fr_a[1]
+        assert round(fr_p[0] - fr_a[0], 4) == 0
+        assert fr_p[0] > 0
+
+    # check error message for number of samples
+    dm = np.array([testdata.bin_data]).reshape((500, 50))
+    df = pd.DataFrame.from_records(dm)
+    sets = [[2, 3, 4, 5, 6, 7, 8]]
+    with pytest.raises(RuntimeError, match="Not enough samples"):
+        ci_estimator.test(df, {x}, {y}, set(sets[0]))
+
+
+def binary_scm(n_samples=200):
+    # We construct a SCM where X1 -> Y <- X and Y -> Z
+    # so X1 is independent from X, but conditionally dependent
+    # given Y or Z
+    rng = np.random.default_rng(seed)
+
+    X = rng.binomial(1, 0.3, (n_samples, 1))
+    X1 = rng.binomial(1, 0.6, (n_samples, 1))
+    Y = X * X1
+    Z = Y + (1 - rng.binomial(1, 0.5, (n_samples, 1)))
+
+    # create input for the CI test
+    df = pd.DataFrame(np.hstack((X, X1, Y, Z)), columns=["x", "x1", "y", "z"])
+    return df
+
+
+def test_g_binary_simulation():
+    """Test G^2 test for binary data."""
+    rng = np.random.default_rng(seed)
+    n_samples = 500
+    df = binary_scm(n_samples=n_samples)
+    for i in range(10):
+        df[i] = rng.binomial(1, p=0.5, size=n_samples)
+    ci_estimator = GSquareCITest(data_type="binary")
+
+    _, pvalue = ci_estimator.test(df, {"x"}, {"y"})
+    assert pvalue < 0.05
+    _, pvalue = ci_estimator.test(df, {"x1"}, {"y"})
+    assert pvalue < 0.05
+    _, pvalue = ci_estimator.test(df, {"x"}, {"x1"})
+    assert pvalue > 0.05
+    _, pvalue = ci_estimator.test(df, {"x1"}, {0})
+    assert pvalue > 0.05
+
+    _, pvalue = ci_estimator.test(df, {"x"}, {"x1"}, {"y"})
+    assert pvalue < 0.05
+
+
+def test_g_binary_highdim():
+    """Test G^2 test for binary data."""
+    rng = np.random.default_rng(seed)
+    n_samples = 1000
+    df = binary_scm(n_samples=n_samples)
+    for i in range(10):
+        df[i] = rng.binomial(1, p=0.8, size=n_samples)
+    ci_estimator = GSquareCITest(data_type="binary")
+
+    _, pvalue = ci_estimator.test(df, {"x"}, {"x1"}, set(range(6)))
+    assert pvalue > 0.05
+    _, pvalue = ci_estimator.test(df, {"x"}, {"y"}, set(range(5)).union({"x1"}))
+    assert pvalue < 0.05
